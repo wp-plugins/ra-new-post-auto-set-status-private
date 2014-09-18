33925c0dfad837088c83d6a502b0a70035c36a51
skuramoto <skuramoto@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-18 10:45:34 +0000

    1st release
    git-svn-id: https://plugins.svn.wordpress.org/ra-new-post-auto-set-status-private/trunk@992487 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ra-newpost-status-auto-private.php
@@ -0,0 +1,42 @@
+<?php 
+/*
+Plugin Name: RA - New Post Auto Set Status "Private"
+Description: The status of the post exhibited directly from new post is compulsorily changed into "private".
+Author: Rasin (skuraomoto)
+Author URI: http://www.rains.jp/
+Version: 1.0.0
+License: GPLv2
+Text Domain: rains-npassp
+Domain Path: /languages/
+*/
+/*
+  Copyright 2013 Rains
+  
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+$ra_npassp_lang = dirname(plugin_basename(__FILE__)) . "/languages/";
+load_plugin_textdomain("rains-npassp", false, $ra_npassp_lang);
+
+function ra_post_status_auto_private() {
+
+	global $post, $wpdb;
+	
+    $wpdb->update($wpdb->posts, array('post_status' => 'private'), array('ID' => $post->ID));
+	clean_post_cache($post->ID);
+	
+}
+add_action('new_to_publish', 'ra_post_status_auto_private');
+add_action('draft_to_publish', 'ra_post_status_auto_private');
+add_action('auto-draft_to_publish', 'ra_post_status_auto_private');
---
+++ readme.txt
@@ -0,0 +1,21 @@
+=== RA - New Post Auto Set Status "Private" ===
+Contributors: Rasin (skuraomoto)
+Tags: rains, PostStatus, Default, Private, Publish, Japanese
+Requires at least: 4.0
+Tested up to: 4.0
+Stable tag: 1.0.0
+
+The status of the post exhibited directly from new post is compulsorily changed into "private".
+
+== Description ==
+The status of the post exhibited directly from new post is compulsorily changed into "private".
+
+== Installation ==
+1. Upload the "ra-newpost-status-auto-private" folder to the "/wp-content/plugins/" directory.
+2. Activate the plugin through the Plugins menu in WordPress.
+
+== Changelog ==
+
+= 1.0.0 =
+* Registration to the plug-in directory.
+* First release.
